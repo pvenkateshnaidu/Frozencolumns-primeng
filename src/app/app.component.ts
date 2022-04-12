21c0c297b134dd5d0d3764588d0c3fe41939d9a1
venkatesh <venkatesh.dev21@gmail.com>
2022-04-12 22:12:42 +0530

    Frozen Scroll with row Height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import { CarService } from './carservice';
 import { Car } from './car';
 import {MessageService} from 'primeng/api';
-
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,39 +11,66 @@ import {MessageService} from 'primeng/api';
 })
 export class AppComponent { 
     cars: Car[];
-
+      
     cols: any[];
 
     selectedCars3: Car[];
 
     frozenColsWidth: any = '0px';
 
-    frozenCols: any = [{ field: 'year', header: 'Year' },{ field: 'brand', header: 'Brand' }];
+    frozenCols: any = [{ field: 'year', header: 'Year', frozen_col: true },{ field: 'brand', header: 'Brand' , frozen_col: true}];
 
     constructor(private carService: CarService, private messageService: MessageService) { }
-
+  
     ngOnInit() {
-        this.carService.getCarsSmall().then(cars => this.cars = cars);
-
+        this.carService.getCarsSmall().then(cars => {
+            this.cars = cars;
+            this.makeRowsSameHeight()
+        });
+       
         this.cols = [
-            { field: 'vin', header: 'Vin' },           
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' },
-            { field: 'vin', header: 'Vin' },
-            { field: 'vin', header: 'Vin' },           
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' },
-            { field: 'vin', header: 'Vin' },
-            { field: 'vin', header: 'Vin' },           
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' },
-            { field: 'vin', header: 'Vin' },
-            { field: 'vin', header: 'Vin' },           
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' },
-            { field: 'vin', header: 'Vin' },
           
+          
+            { field: 'year', header: 'year'  },
+            { field: 'vin', header: 'Vin' },
+            { field: 'vin', header: 'Vin' },           
+            { field: 'brand', header: 'Brand' },
+            { field: 'color', header: 'Color' },
+            { field: 'vin', header: 'Vin' },
+            { field: 'vin', header: 'Vin' },           
+            { field: 'brand', header: 'Brand' },
+            { field: 'color', header: 'Color' },
+            { field: 'vin', header: 'Vin' },
+            { field: 'vin', header: 'Vin' },           
+            { field: 'brand', header: 'Brand' },
+            { field: 'color', header: 'Color' },
+            { field: 'vin', header: 'Vin' },
+            { field: 'vin', header: 'Vin' },
+            { field: 'vin', header: 'Vin' },
+            { field: 'year', header: 'Year' }
         ];
     }
+    makeRowsSameHeight() {
+        setTimeout(() => {
+          if (jQuery('.ui-table-scrollable-wrapper').length) {
+            let wrapper = $('.ui-table-scrollable-wrapper');
+            wrapper.each(function () {
+              let w = $(this);
+              let frozen_rows: any = w.find('.ui-table-frozen-view tr');
+              let unfrozen_rows = w.find('.ui-table-unfrozen-view tr');
+              for (let i = 0; i < frozen_rows.length; i++) {
+                if (frozen_rows.eq(i).height() > unfrozen_rows.eq(i).height()) {
+                  unfrozen_rows.eq(i).height(frozen_rows.eq(i).height());
+                } else if (frozen_rows.eq(i).height() < unfrozen_rows.eq(i).height()) {
+                  frozen_rows.eq(i).height(unfrozen_rows.eq(i).height());
+                }
+              }
+            });
+          }
+        });
+      }
 
 }
+
+ 
+
